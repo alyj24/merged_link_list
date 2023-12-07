8f61a620885e6a7eeea42b777e4e759137bd1b85
Family <albon.136546120855@2depedqc.ph>
2023-12-07 22:00:44 +0800

    Iterate elements

--- merge_two_sorted_linked_lits.py
+++ merge_two_sorted_linked_lits.py
@@ -6,6 +6,22 @@ class ListNode:
         self.next = next
 
 def merge_sorted_lists(list1, list2):
-    # Create a dummy node to simplify the code
     lists = ListNode()
     current = lists
+
+    while list1 is not None and list2 is not None:
+        if list1.value <= list2.value:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+
+        current = current.next
+
+        if list1 is not None:
+            current.next = list1
+        elif list2 is not None:
+            current.next = list2
+
+        return lists.next
